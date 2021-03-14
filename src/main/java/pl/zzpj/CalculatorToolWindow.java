4e9af03cbd8151efea50d642c5441cc97a72f065
Artur <artur300998@gmail.com>
2021-03-14 19:56:24 +0100

    Simple calculator layout.

--- src/main/java/pl/zzpj/CalculatorToolWindow.java
+++ src/main/java/pl/zzpj/CalculatorToolWindow.java
@@ -4,7 +4,27 @@ import javax.swing.*;
 
 public class CalculatorToolWindow {
     private JPanel grid;
-    private JButton button1;
+    private JButton a7Button;
+    private JButton a4Button;
+    private JButton a8Button;
+    private JButton a1Button;
+    private JButton a9Button;
+    private JButton a6Button;
+    private JButton a3Button;
+    private JButton a5Button;
+    private JButton a2Button;
+    private JButton plusMinusButton;
+    private JButton a0Button;
+    private JButton dotButton;
+    private JTextField textField;
+    private JButton backspaceButton;
+    private JButton subButton;
+    private JButton addButton;
+    private JButton equalsButton;
+    private JButton cButton;
+    private JButton divButton;
+    private JButton mulButton;
+    private JButton percentButton;
 
     public CalculatorToolWindow() {
     }
